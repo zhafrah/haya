2437fbb146a89460f0cddc034694d9aa3b9d3390
Haya <zhayadest@gmail.com>
2026-06-04 12:02:35 +0700

    change: php code

---
+++ index.php
@@ -0,0 +1,51 @@
+<html> 
+<head>
+    <title>RIIZE - Home</title>
+    <link rel="stylesheet" href="assets/css/style.css"
+    
+</head>
+
+<body>
+
+    <nav>
+        <a href="index.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="penutup.html">Penutup</a>
+        <a href="data mahasiswa.html">Data Mahasiswa</a>
+    </nav>
+
+    <h1 align="center">
+        Makna Filosofis di Balik Nama RIIZE
+    </h1>
+    <table border="1" align="center" cellspacing="0" cellpadding="10px">
+        <tr>
+            <td>
+                <a href="index.html">Home</a>
+            </td>
+            <td>
+                <a href="about.html">About</a>
+            </td>
+            <td>
+                <a href="penutup.html">Penutup</a>
+            </td>
+            <td>
+                <a href="mahasiswa.html">Data Mahasiswa</a>
+            </td>
+        </tr>
+    </table>
+    <center>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSklpXEh2WWbZmcz8MCrXvK1xw4ZqOyFECL7g&s" 
+    width="200px"/>
+     </center>
+    <p>
+        SM Entertainment kembali menyita perhatian penggemar K-Pop. 
+        Sebuah boy group akan didebutkan pada September 2023 mendatang.
+    </p>
+
+    <p>
+        Popularitas SM Entertainment dalam melahirkan grup seperti SHINee, EXO, NCT, dan aespa 
+        membuat publik sangat menantikan kehadiran grup baru ini.
+    </p>
+
+</body>
+</html>
---
+++ penutup.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <title>RIIZE - Penutup</title>
+    <link rel="stylesheet" href="assets/css/style.css"
+
+</head>
+
+<body>
+
+    <!-- NAVBAR DI ATAS -->
+    <nav>
+        <a href="index.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="penutup.html" class="active">Penutup</a>
+    </nav>
+
+    <h1>Debut dan Harapan</h1>
+
+    <p>
+        RIIZE akan membuka akun Instagram resmi pada 1 Agustus dan memperkenalkan seluruh anggotanya.
+    </p>
+
+    <p>
+        Grup ini diharapkan membawa dampak besar di industri K-Pop dengan karya-karya mereka.
+    </p>
+
+    <p>
+        RIIZE menjadi grup rookie baru setelah aespa dan melanjutkan generasi boy group SM setelah NCT.
+    </p>
+
+</body>
+</html>
